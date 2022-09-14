5ae902a5aec9512d12d259754335a0b199ee362d
Nico Gaytan <nicholas.j.gaytan97@gmail.com>
2022-09-13 23:46:51 -0500

    transferring most design to seperate css page

--- src/engine.js
+++ src/engine.js
@@ -1,17 +1,10 @@
 $(document).click(function(e) {
     e.preventDefault();
-
-    var home = document.getElementById('home');
-    var projects = document.getElementById('projects');
-
     
     home.click(function(e) {
         // e.preventDefault();
         $(document).scrollTop(100);
     })
 
-    projects.click(function(e) {
-        $(document).scrollTo('.projects')
-    })
 
 })
